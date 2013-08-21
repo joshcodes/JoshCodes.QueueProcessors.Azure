fb9b24f73266862e79687da0a5883a43911ba9d7
Josh <josh@magicmoments.com>
2013-08-21 15:21:38 -0400

    settings now come form app or web.config or azure

--- Settings.cs
+++ Settings.cs
@@ -12,9 +12,9 @@ namespace JoshCodes.QueueProcessors.Azure
         {
             get
             {
-                var serviceBusConnectionString = 
-                    "Endpoint=sb://magicmoments-api.servicebus.windows.net/;SharedSecretIssuer=owner;SharedSecretValue=4AcW4dp63TJoEtm8WOwlMNscRpvzChhheB6Qzq+CobA=";
-                    //  Microsoft.WindowsAzure.CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+                var serviceBusConnectionString = System.Configuration.ConfigurationManager.AppSettings["azure.service_bus-connection_string"];
+                // "Endpoint=sb://magicmoments-api.servicebus.windows.net/;SharedSecretIssuer=owner;SharedSecretValue=4AcW4dp63TJoEtm8WOwlMNscRpvzChhheB6Qzq+CobA=";
+                //  Microsoft.WindowsAzure.CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
                 return serviceBusConnectionString;
             }
         }
